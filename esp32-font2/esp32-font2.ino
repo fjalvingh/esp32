628de86fc3f6bb3a08be2e230d1dda23ebdcfe6b
Frits Jalvingh <jal@etc.to>
2020-07-02 00:15:39 +0200

    pacman and ghosts

--- esp32-font2/esp32-font2.ino
+++ esp32-font2/esp32-font2.ino
@@ -2,9 +2,9 @@
 #include <Adafruit_GFX.h>
 
 #include <Fonts/FreeSerif9pt7b.h>
-#include "pacman2_1.h"
-#include "pacman2_2.h"
-#include "pacman2_4.h"
+//#include "pacman2_1.h"
+//#include "pacman2_2.h"
+//#include "pacman2_4.h"
 
 #define LED_PIN     27
 #define COLOR_ORDER BRG
@@ -36,11 +36,91 @@ struct FRAMESET {
   const byte** frames;
 };
 
-const byte* pacman_framelist[] = { pacman1, pacman2, pacman1, pacman4 };
+struct FRAMESET2 {
+  int width;
+  int height;
+  int framecount;
+  const int** palette;
+  const byte** frames;
+};
 
-const FRAMESET pacman = {12, 13, 4, CRGB::Yellow, pacman_framelist };
+struct ANIMITEM {
+  FRAMESET2* frameset;
+  int y;
+  int relx;
+};
+
+//static const byte* pacman_framelist[] = { pacman1, pacman2, pacman1, pacman4 };
+//
+//static const FRAMESET pacman = {12, 13, 4, CRGB::Yellow, pacman_framelist };
+
+
+/**
+ * Ghosts: 2bit depth indexed bitmap.
+ */
+static const unsigned char ghost1_2_bm[] = {
+0x0,0x14,0x0,0x1,0x55,0x40,0x5,0x55,0x50,0x6,0x96,0x90,0x1b,0xda,0xf4,0x1b
+,0xda,0xf4,0x5a,0x56,0x95,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55
+,0x55,0x55,0x55,0x55,0x15,0x15,0x14,0x14,0x14,0x14};
+static const int ghost1_2_col[] = {0x0, 0xd95763, 0xffffff, 0x0};
+static const unsigned char ghost1_1_bm[] = {
+0x0,0x14,0x0,0x1,0x55,0x40,0x5,0x55,0x50,0x6,0x96,0x90,0x1b,0xda,0xf4,0x1b
+,0xda,0xf4,0x5a,0x56,0x95,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55
+,0x55,0x55,0x55,0x55,0x51,0x51,0x51,0x40,0x40,0x40};
+static const int ghost1_1_col[] = {0x0, 0xd95763, 0xffffff, 0x0};
+static const unsigned char ghost1_3_bm[] = {
+0x0,0x14,0x0,0x1,0x55,0x40,0x5,0x55,0x50,0x6,0x96,0x90,0x1b,0xda,0xf4,0x1b
+,0xda,0xf4,0x5a,0x56,0x95,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55
+,0x55,0x55,0x55,0x55,0x55,0x15,0x15,0x14,0x4,0x5};
+static const int ghost1_3_col[] = {0x0, 0xd95763, 0xffffff, 0x0};
+
+
+static const unsigned char pacman2_4_bm[] = {
+0x0,0x15,0x40,0x1,0x55,0x54,0x5,0x55,0x55,0x15,0x55,0x55,0x15,0x55,0x55,0x55
+,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x55,0x15,0x55,0x55,0x15,0x55,0x55,0x5,0x55
+,0x55,0x1,0x55,0x54,0x0,0x15,0x40};
+static const int pacman2_4_col[] = {0x0, 0xfbf236};
+static const unsigned char pacman2_1_bm[] = {
+0x0,0x15,0x40,0x1,0x55,0x54,0x5,0x55,0x55,0x15,0x55,0x55,0x15,0x55,0x50,0x55
+,0x54,0x0,0x55,0x0,0x0,0x55,0x54,0x0,0x15,0x55,0x50,0x15,0x55,0x55,0x5,0x55
+,0x55,0x1,0x55,0x54,0x0,0x15,0x40};
+static const int pacman2_1_col[] = {0x0, 0xfbf236};
+static const unsigned char pacman2_2_bm[] = {
+0x0,0x15,0x40,0x1,0x55,0x50,0x5,0x55,0x40,0x15,0x55,0x0,0x15,0x54,0x0,0x55
+,0x50,0x0,0x55,0x40,0x0,0x55,0x50,0x0,0x15,0x54,0x0,0x15,0x55,0x0,0x5,0x55
+,0x40,0x1,0x55,0x50,0x0,0x15,0x40};
+static const int pacman2_2_col[] = {0x0, 0xfbf236};
+static const unsigned char pacman2_3_bm[] = {
+0x0,0x15,0x40,0x1,0x55,0x54,0x5,0x55,0x55,0x15,0x55,0x55,0x15,0x55,0x50,0x55
+,0x54,0x0,0x55,0x0,0x0,0x55,0x54,0x0,0x15,0x55,0x50,0x15,0x55,0x55,0x5,0x55
+,0x55,0x1,0x55,0x54,0x0,0x15,0x40};
+static const int pacman2_3_col[] = {0x0, 0xfbf236};
+static const unsigned char* pacman_frames[] = {pacman2_1_bm,pacman2_2_bm,pacman2_3_bm,pacman2_4_bm};
+static const int* pacman_palette[] = {pacman2_1_col,pacman2_2_col,pacman2_3_col,pacman2_4_col};
+static FRAMESET2 pacman = { 12, 13, 4, pacman_palette, pacman_frames };
+
+
+
+
+
+static const int* ghost1_colset[] { ghost1_1_col, ghost1_2_col, ghost1_3_col };
+static const byte* ghost1_frameset[] = { ghost1_1_bm, ghost1_2_bm, ghost1_3_bm };
+
+static FRAMESET2 ghost1 = { 12, 14, 3, ghost1_colset, ghost1_frameset };
+
+static const int ghost2_col[] = {0x0, 0x5b6ee1, 0xffffff, 0x0};
+static const int* ghost2_colset[] { ghost2_col, ghost2_col, ghost2_col };
+static FRAMESET2 ghost2 = { 12, 14, 3, ghost2_colset, ghost1_frameset };
+
 
 int pacman_frame = 0;
+int ghost_frame = 0;
+
+static const ANIMITEM animList[] = {
+  { &pacman, 0, 0 },                  // Start with pacman
+  { &ghost1, 0, -40 },
+  { &ghost2, 0, -54 }
+};
 
 
 void setup() {
@@ -61,6 +141,7 @@ void setRowLeds(int row, CRGB color) {
 }
 
 void loop() {
+  
   render81();
   FastLED.show();
   delay(5000);
@@ -69,41 +150,48 @@ void loop() {
     renderPacFrame(x);
   }
 
-  
-//  const GFXfont* font = &FreeSerif9pt7b;
-//  int w = renderChar(font, 0, 0, '8');
-//  renderChar(font, w, 0, '1');
+  for(int x = -16; x < 17; x++) {
+    renderGhostFrame(&ghost1, x);
+  }
+  for(int x = -16; x < 17; x++) {
+    renderGhostFrame(&ghost2, x);
+  }
+  delay(10000);
+}
 
-//  render1Bits(pacman1, 12, 13, 0, 0, CRGB::Yellow);
-//  FastLED.show();
-//
-//  memset(leds, 0, sizeof(leds));
-//  delay(200);
-//  render1Bits(pacman2, 12, 13, 0, 0, CRGB::Yellow);
-//  FastLED.show();
-//  
-//  memset(leds, 0, sizeof(leds));
-//  delay(200);
-//  render1Bits(pacman1, 12, 13, 0, 0, CRGB::Yellow);
-//  FastLED.show();
-//
-//  memset(leds, 0, sizeof(leds));
-//  delay(200);
-//  render1Bits(pacman4, 12, 13, 0, 0, CRGB::Yellow);
-//  FastLED.show();
-//
-//  memset(leds, 0, sizeof(leds));
-//  delay(200);
+void renderAnimationItemList(ANIMITEM* itemList, int itemCount) {
+  //-- calculate how many x'es we need
+  int minx = 999999, maxx = -999999;
+
+  for(int i = 0; i < itemCount; i++) {
+    ANIMITEM item = itemList[i];
+    int sx = item->relx - item->frameset->width;
+    if(sx < minx)
+      minx = sx;
+    int ex = item-relx;
+    if(ex > maxx)
+      maxx = ex;
+  }
+
+  
+  
+  
 }
 
 void renderPacFrame(int x) {
   memset(leds, 0, sizeof(leds));
   render81();
-  renderFrame(&pacman, x, 0, pacman_frame);
+  render2Frame(&pacman, x, 0, pacman_frame);
   FastLED.show();
   delay(100);
-  
-  
+}
+
+void renderGhostFrame(FRAMESET2* ani, int x) {
+  memset(leds, 0, sizeof(leds));
+  render81();
+  render2Frame(ani, x, 0, ghost_frame);
+  FastLED.show();
+  delay(50);
 }
 
 void render81() {
@@ -116,6 +204,7 @@ void render81() {
 void setLed(int x, int y, CRGB color) {
   if(x < 0 || x >= 16 || y < 0 || y >= 16) 
     return;
+  y = 15 - y;
 
   int panelRowIndex = x % 8;
   int xPanelSelect = ((~x >> 3) & 0x1);          // 0 = panel 1, 1 - panel 0
@@ -133,6 +222,39 @@ void setLed(int x, int y, CRGB color) {
   }
 
   leds[apx] = color;
+}
+
+void render2Bits(const byte* pattern, int patwidth, int patheight, int x, int y, const int* palette) {
+  const byte* src = pattern;
+  for(int py = 0; py < patheight; py++) {
+    int bitsleft = 0;
+    int byva = 0;
+    for(int px = 0; px < patwidth; px++) {
+      if(bitsleft <= 0) {
+        byva = (*src++) & 0xff;
+        bitsleft = 6;
+      } else {
+        bitsleft-= 2;
+      }
+      int index = (byva >> 6) & 0x3;
+      if(index > 0) {
+        int color = palette[index];
+//        Serial.print("col ");
+//        Serial.print("x="); Serial.print(px, DEC);
+//        Serial.print(" x="); Serial.print(py, DEC);
+//        Serial.print(" color="); Serial.print(color, HEX);
+//        Serial.println();
+        setLed(x + px, y + py, color);
+      }
+      byva = byva << 2;
+    }
+  }
+}
+
+void render2Frame(const FRAMESET2* set, int x, int y, int& currentFrame) {
+  render2Bits(set->frames[currentFrame], set->width, set->height, x, y, set->palette[currentFrame]);
+  if(++currentFrame >= set->framecount)
+    currentFrame = 0;
 }
 
 void render1Bits(const byte* pattern, int patwidth, int patheight, int x, int y, CRGB color) {
@@ -173,9 +295,9 @@ int renderChar(const GFXfont* fi, int xoff, int yoff, char c) {
   int cWidth = gly.width;
   int coff = gly.bitmapOffset;
 
-  char buf[80];
-  sprintf(buf, "off=%d, gw=%d, gh=%d", coff, gly.width, gly.height);
-  Serial.println(buf);
+//  char buf[80];
+//  sprintf(buf, "off=%d, gw=%d, gh=%d", coff, gly.width, gly.height);
+//  Serial.println(buf);
 
   const byte* cptr = fi->bitmap + coff;
   int bl = 0;
@@ -190,7 +312,7 @@ int renderChar(const GFXfont* fi, int xoff, int yoff, char c) {
       bl--;
       int pel = data & 0x80;
       data = data << 1;
-      setLed(xoff + cx, 15 - (yoff + cy), pel == 0 ? CRGB::Black : CRGB::White);
+      setLed(xoff + cx, (yoff + cy), pel == 0 ? CRGB::Black : CRGB::White);
     }
   }
 
